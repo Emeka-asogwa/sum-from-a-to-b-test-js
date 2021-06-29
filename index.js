45fa83aca9310be98a02d19205d4b506e976a232
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-29 04:25:38 +0000

    ✍🏽 initial commit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let count = fromN;
+
+  for (let i = fromN; i < toN; i++) {
+    count = count + i + 1;
+
+  }
+
+  return count;
+};
+
+console.log(sum(3, 7));
 
 module.exports = sum;
